a0f456ef302e525aff1d35485347122ed3fe0c21
Sergey Gusarov <laborer2008@gmail.com>
2015-05-07 22:52:13 +0000

    installLibfreenect2.sh: Remove leftovers from previous builds

--- extras/installLibfreenect2.sh
+++ extras/installLibfreenect2.sh
@@ -5,17 +5,24 @@
 #opencv-dev -> opencv4tegra-dev -> already installed from Jetpack
 sudo apt-get install -y build-essential libturbojpeg libtool autoconf libudev-dev cmake mesa-common-dev freeglut3-dev libxrandr-dev doxygen libxi-dev libjpeg-turbo8-dev
 
+# Remove leftovers from previous builds
+rm -rf libfreenect2
+
 # Get libfreenect2
 git clone https://github.com/jetsonhacks/libfreenect2.git
+
 # Copy nv_headers and turbojepg.h into libfreenect2/depends
 wget http://developer.download.nvidia.com/mobile/tegra/l4t/r21.2.0/sources/gstjpeg_src.tbz2
 tar -xvf gstjpeg_src.tbz2 gstjpeg_src/nv_headers
 mv gstjpeg_src/nv_headers/ libfreenect2/depends/
+
 # Cleanup the empty folder
 rmdir gstjpeg_src/
+
 # build libusb and glfw
 cd libfreenect2/depends
 sh install_ubuntu.sh
+
 #fix up libturbojpeg.so linker doesn't get upset
 sudo ln -s /usr/lib/arm-linux-gnueabihf/libturbojpeg.so.0.0.0 /usr/lib/arm-linux-gnueabihf/libturbojpeg.so
 
