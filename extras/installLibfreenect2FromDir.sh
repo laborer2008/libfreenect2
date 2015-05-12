9ae418baf6249163ba6a7d0857e8ae494e72708a
Sergey Gusarov <laborer2008@gmail.com>
2015-05-12 20:44:28 +0000

    extras/installLibfreenect2FromDir.sh: Restored correct L4T 21.2 download path of gstjpeg_src.tbz2

--- extras/installLibfreenect2FromDir.sh
+++ extras/installLibfreenect2FromDir.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y build-essential libturbojpeg libtool autoconf libudev-de
     freeglut3-dev libxrandr-dev doxygen libxi-dev libjpeg-turbo8-dev checkinstall
 
 # Copy nv_headers into libfreenect2/depends/gstjpeg_src
-wget http://developer.download.nvidia.com/embedded/L4T/r21_Release_v3.0/sources/gstjpeg_src.tbz2
+wget http://developer.download.nvidia.com/mobile/tegra/l4t/r21.2.0/sources/gstjpeg_src.tbz2
 tar -xvf gstjpeg_src.tbz2 gstjpeg_src/nv_headers
 
 rm -rf $LIBFREENECT2_DIR/depends/gstjpeg_src
